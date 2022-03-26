1a22ff1160a3cf7ce47c39093a438ee226d15b21
arifraj199 <karif9514@gmail.com>
2022-03-26 13:57:55 +0600

    readme file edit

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -6,11 +6,21 @@ const Questions = () => {
     <div className="questions-container">
         <div className="questions">
             <h2>How React Works?</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel blanditiis, consequuntur obcaecati in voluptatem voluptate pariatur a aut minus sequi atque libero voluptas. Modi praesentium dignissimos corrupti! Totam, libero temporibus!</p>
+            <p>React is a Javascript library.It has a structure and the data flow has clearly defined rule.Each code perform on its own inside component.Every component are separated from another but communicate with each other to make application work.We can set data dynamically in react.It's more easy to write code and give best performance from plain JS code.<br/>
+            Each react component output is given in JSX format which stand for Javascript XML and that allows write html in react.</p>
         </div>
         <div className="questions">
             <h2>Difference Between Props and State?</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel blanditiis, consequuntur obcaecati in voluptatem voluptate pariatur a aut minus sequi atque libero voluptas. Modi praesentium dignissimos corrupti! Totam, libero temporibus!</p>
+            <p>
+                props are read only <br/>
+                where,state is both read and write<br/><br/>
+
+                props changes can be synchronously <br/>
+                but state changes can be asynchronously<br/><br/>
+
+                props can not be modified <br/>
+                in other word,state can be modified using this.setState. 
+            </p>
         </div>
     </div>
   );
